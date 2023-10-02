8d050f45312c394792d908f77aa9efa1706ee209
minhbear <minhbeardev@gmail.com>
2023-10-03 00:33:36 +0700

    feat: api for cru gallery

--- src/film/dtos/index.ts
+++ src/film/dtos/index.ts
@@ -1,15 +1,19 @@
 import { ObjectType, OmitType, InputType, Field, Int, GraphQLISODateTime } from '@nestjs/graphql'
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
 import { Paginated } from '@/common/interface'
 import { FilmEntity } from '@/db/entities/film'
 import { FILM_GENRE } from '@/common/constant'
 import { FilmTopCast } from '@/common/types'
+import { FilmGalleryEntity } from '@/db/entities/filmGalery'
 
 @ObjectType({ isAbstract: true })
-export class FilmInformationPublic extends OmitType(FilmEntity, ['whiteList', 'gallery']) {}
+export class FilmInformationPublic extends OmitType(FilmEntity, ['whiteList']) {}
 
 @ObjectType({ isAbstract: true })
 export class PaginatedFilm extends Paginated(FilmInformationPublic) {}
+
+@ObjectType({ isAbstract: true })
+export class PaginatedFilmGallery extends Paginated(FilmGalleryEntity) {}
 
 @InputType({ isAbstract: true })
 export class CreateFilmDto {
@@ -48,4 +52,25 @@ export class CreateFilmDto {
   @Field(() => GraphQLISODateTime)
   @IsNotEmpty()
   endDateSubscriber: Date
+}
+
+@InputType({ isAbstract: true })
+export class UpsertGalleryFilmDto {
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsNumber()
+  id?: number | null
+
+  @Field()
+  @IsString()
+  name: string
+
+  @Field()
+  @IsString()
+  url: string
+
+  @Field()
+  @IsNotEmpty()
+  @IsNumber()
+  filmId: number
 }
--- src/film/film.service.ts
+++ src/film/film.service.ts
@@ -1,21 +1,25 @@
 import { FilmEntity } from '@/db/entities/film'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { Repository, getManager } from 'typeorm'
 import { GetFilmCommand } from './commands/GetFilm.command'
 import { plainToClass } from 'class-transformer'
-import { CreateFilmDto, FilmInformationPublic, PaginatedFilm } from './dtos'
+import { CreateFilmDto, FilmInformationPublic, PaginatedFilm, PaginatedFilmGallery, UpsertGalleryFilmDto } from './dtos'
 import { PaginationArgs } from '@/common/interface'
 import { paginate } from '@/common/paginate'
 import { PersonEntity } from '@/db/entities/person'
 import { ReturnMessageBase } from '@/common/interface/returnBase'
 import { ADMIN_PROCESS_STATUS, FILM_STATUS } from '@/common/constant'
+import { FilmGalleryEntity } from '@/db/entities/filmGalery'
+import { compactObject } from '@/utils'
 
 @Injectable()
 export class FilmService {
   constructor(
     @InjectRepository(FilmEntity)
-    private readonly filmRepository: Repository<FilmEntity>
+    private readonly filmRepository: Repository<FilmEntity>,
+    @InjectRepository(FilmGalleryEntity)
+    private readonly filmGalleryRepository: Repository<FilmGalleryEntity>
   ) {}
 
   async geById(id: number): Promise<FilmInformationPublic> {
@@ -30,6 +34,20 @@ export class FilmService {
     return paginate({ query, paginationArgs, isUsedPlainClass: true, classRef: FilmEntity, defaultLimit: 7 })
   }
 
+  async getGalleryOfFilm(params: {filmId: number, paginationArgs: PaginationArgs, person: PersonEntity}): Promise<PaginatedFilmGallery> {
+    const { filmId, paginationArgs, person } = params
+    // validate the current user have the permission to see via nft or not 
+    
+    await GetFilmCommand.getFilmById(filmId)
+
+    const query = this.filmGalleryRepository
+      .createQueryBuilder()
+      .select()
+      .where('FilmGalleryEntity.filmId = :filmId', { filmId })
+
+    return paginate<FilmGalleryEntity>({ query, paginationArgs, defaultLimit: 10 })
+  }
+
   async createFilm(input: CreateFilmDto, person: PersonEntity): Promise<ReturnMessageBase> {
     // Minh-27-9-2023: we add admin status approved by default to skip the step approval by admin. This status need to update bu admin
     await this.filmRepository.save({
@@ -41,6 +59,23 @@ export class FilmService {
 
     return {
       message: `Create film success`,
+      success: true
+    }
+  }
+
+  async upsertGalleryFilm(input: UpsertGalleryFilmDto, person: PersonEntity): Promise<ReturnMessageBase> {
+    await GetFilmCommand.getByFilmIdAndPersonId(input.filmId, person.id)
+
+    await getManager()
+      .createQueryBuilder()
+      .insert()
+      .into(FilmGalleryEntity, ['id', 'name', 'url', 'filmId'])
+      .values(compactObject(input))
+      .orUpdate(['name', 'url'], ['id'], { skipUpdateIfNoValuesChanged: true })
+      .execute()
+
+    return {
+      message: `Upsert gallery for film success`,
       success: true
     }
   }
